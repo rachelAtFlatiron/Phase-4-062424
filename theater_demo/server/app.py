89026b04cec66c00d05e51d87f9d6b833c977451
Rachel Katz <rachel.katz@flatironschool.com>
2024-04-23 15:50:18 -0400

    removed solution

--- theater_demo/server/app.py
+++ theater_demo/server/app.py
@@ -18,7 +18,7 @@ def index():
     return '<h1>Hello World!</h1>'
 
 @app.route('/productions')
-def Productions():
+def all_productions():
     q = Production.query.all()
     prod_list = [p.to_dict() for p in q]
 
@@ -26,7 +26,7 @@ def Productions():
     return res 
 
 @app.route('/productions/<int:id>')
-def One_Production(id):
+def production_by_id(id):
     q = Production.query.filter_by(id=id).first()
 
     prod_dict = q.to_dict()
@@ -34,22 +34,6 @@ def One_Production(id):
     return res
 
 # ✅ 1c. Create a `/roles` route to see the other side of the one-to-many relationship
-@app.route('/roles')
-def all_roles():
-    q = Role.query.all()
-
-    role_dict = q.to_dict()
-    res = make_response(jsonify(role_dict), 200)
-    return res
-
 # ✅ 3b. Create routes to access all actors and all productions
-@app.route('/actors')
-def all_actors():
-    q = Actor.query.all()
-
-    actor_dict = q.to_dict()
-    res = make_response(jsonify(actor_dict), 200)
-    return res
-
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- theater_demo/server/models.py
+++ theater_demo/server/models.py
@@ -20,50 +20,19 @@ class Production(db.Model, SerializerMixin):
     description = db.Column(db.String) 
     composer = db.Column(db.String)
 
-    roles = db.relationship('Role', back_populates='production')
-    actors = association_proxy('roles', 'actor')
-    
-    serialize_rules = ('-created_at', '-updated_at', '-roles.production', '-actors.productions')
 
 # ✅ 1. Create a Role model
-class Role(db.Model, SerializerMixin):
-    __tablename__ = "roles"
-    
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    role_name = db.Column(db.String) 
     # ✅ 1a. Create a one-to-many relationship between `Role` and `Production`
     # ✅ 1b. Fetch the route `/productions` to see all the roles associated with each production
     # ✅ 1c. Create a `/roles` route to see the other side of the one-to-many relationship
-    production_id = db.Column(db.Integer, db.ForeignKey('productions.id')) 
-    production = db.relationship('Production', back_populates='roles')
 
-    actor_id = db.Column(db.Integer, db.ForeignKey('actors.id')) 
-    actor = db.relationship('Actor', back_populates='roles')
-
-    serialize_rules = ('-created_at', '-updated_at', '-production.roles', '-actors.roles')
 
 # ✅ 2. Create an `Actor` model
-class Actor(db.Model, SerializerMixin):
-    __tablename__ = "actors"
-
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
-    name = db.Column(db.String) 
-    image = db.Column(db.String)
-    age = db.Column(db.Integer)
-    country = db.Column(db.String)
 
     # ✅ 2a. Create a one-to-many relationship between `Role` and `Actor`
     # ✅ 2b. Fetch the route `/roles` to see all the roles associated with each actor
     # ✅ 2c. Create an `/actors` route to see the other side of the one-to-many relationship
-    roles = db.relationship('Role', back_populates='actor')
     # ✅ 3. Create many-to-many relationship between `Production` and `Actor` using `association_proxy`
-    productions = association_proxy('roles', 'production')
     # ✅ 3a. Use `SerializerMixin` to prevent max recursion
     # ✅ 3b. Create routes to access all actors and all productions
-    serialize_rules = ('-created_at', '-updated_at', '-roles.actor', '-productions.actors')
+
