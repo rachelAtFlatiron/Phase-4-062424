1d5f0963cfb8ab334bd19a77bd7a2124db1eb80f
Rachel Katz <rachel.katz@flatironschool.com>
2024-08-27 12:38:16 -0400

    demo done

--- theater_demo/server/models.py
+++ theater_demo/server/models.py
@@ -1,9 +1,28 @@
-# handle creating the database
+# create model (class) that represents our database table
 
-#SQLAlchemy import
+#flask_sqlalchemy is our ORM extension
+from flask_sqlalchemy import SQLAlchemy 
+from sqlalchemy_serializer import SerializerMixin
 
-# 1. ✅ Create a Production Model
+db = SQLAlchemy()
 
-# ✅ 9b. Use serializer rules to remove `created_at` and `updated_at`
+class Production(db.Model, SerializerMixin):
 
-# ✅ 2. navigate to app.py
+    #this is the table name that will show up in resources.db (our SQL)
+    __tablename__ = "productions"
+
+    #columns that will show up in resources.db and that will be accessible as attributes (similarly to our attributes/properties from Phase 3)
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    title = db.Column(db.String)
+    year = db.Column(db.Integer)
+    director = db.Column(db.String)
+    length = db.Column(db.Integer)
+
+    #created_at and updated_at are great to have in the database, however may not necessarily be needed for our app.py routes' responses
+    serialize_rules=('-created_at', '-updated_at')
+
+    def __repr__(self):
+        return f'<Production id={self.id} title={self.title} year={self.year} director={self.director} length={self.length} />'
--- theater_demo/server/seed.py
+++ theater_demo/server/seed.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
-
-# 4a. Imports
-
-# 4b. Create application context `with app.app_context():`
-
-# 4c. Create a query to delete all existing records from Production
+from app import app 
+from models import Production, db
 
 
-# 4d. Create some seeds for production and commit them to the database.
+with app.app_context():
 
-# 5. Check seeds
+    # create some Production class instances 
+    oppenheimer = Production(title="Oppenheimer", year=2023, director="Christopher Nolan", length=180)
+    barbie = Production(title="Barbie", year=2023, director="Greta Gerwig", length=114)
+    killers_of_the_flower_moon = Production(title="Killers of the Flower Moon", year=2023, director="Martin Scorsese", length=206)
+    dune_part_two = Production(title="Dune: Part Two", year=2024, director="Denis Villeneuve", length=155)
+    the_marvels = Production(title="The Marvels", year=2024, director="Nia DaCosta", length=105)
+
+    print(oppenheimer.title)
+    print(barbie.year)
+    # we will save them to the database using SQLAlchemy (aka ORM)
+
+    # db.session.add(oppenheimer)
+    # db.session.add(barbie)
+
+    #staging changes
+    db.session.add_all([oppenheimer, barbie, killers_of_the_flower_moon, dune_part_two, the_marvels])
+
+    #applying changes to database
+    db.session.commit()
+
+    #Production.query.all() => gets all rows as instances of Production 
+    #Production.query.filter_by(id=x).all() or .first()
+        #.all() => return a list
+        #.first() => the first matching row as an instance
